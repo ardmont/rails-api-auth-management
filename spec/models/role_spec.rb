8d01198542c8c5b2a0db4876b59b891b2e3cae72
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-11-04 20:51:38 -0300

    Tests uniqueness of Role's name.

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Role, type: :model do
   it { should have_many(:users) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:level) }
+
+  subject { create(:role) }
+  it { should validate_uniqueness_of(:name) }
+  it { should have_db_index(:name).unique(true) }
 end
