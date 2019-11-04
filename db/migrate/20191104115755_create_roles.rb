983e2fae47afaaada910a1e664e76e3e5347ca29
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-11-04 11:48:30 -0300

    Fix - Change level type to integer.

--- db/migrate/20191104115755_create_roles.rb
+++ db/migrate/20191104115755_create_roles.rb
@@ -2,7 +2,7 @@ class CreateRoles < ActiveRecord::Migration[6.0]
   def change
     create_table :roles do |t|
       t.string :type
-      t.number :level
+      t.integer :level
 
       t.timestamps
     end
