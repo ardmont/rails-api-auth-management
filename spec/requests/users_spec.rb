6020ba9ff90115684c19d55303b9150049f567dd
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-11-05 13:14:39 -0300

    Tests GET /users/:id.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe "Users requests", type: :request do
   let!(:role) { create(:role)}
   let!(:users) { create_list(:user, 45, role_id: role.id) }
   let(:user_sample) { users.sample }
+  let(:user_sample_id) { user_sample.id }
 
   describe "GET /users" do
     it "Should return http status 200" do
@@ -39,5 +40,28 @@ RSpec.describe "Users", type: :request do
 
     end
 
+  end
+
+  describe 'GET /users/:id' do
+    before { get "/users/#{user_sample_id}" }
+
+    context 'When user exists' do
+      it 'Returns user' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(user_sample_id)
+      end
+
+      it 'Returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'When user doesn\'t exists' do
+      let(:user_sample_id) { 100 }
+
+      it 'Returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+    end
   end
 end
