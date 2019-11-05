f1c74c4c07560fd912297e8fafb1ce27f6ef7f0c
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-11-05 12:05:20 -0300

    Tests logs and users pagination.

--- spec/requests/logs_spec.rb
+++ spec/requests/logs_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Logs", type: :request do
+  let!(:logs) { create_list(:log, 45) }
+
   describe "GET /logs" do
     it "Should return http status 200" do
       get logs_path
       expect(response).to have_http_status(200)
     end
+    
+    context 'GET /logs?page=2&per_page=30' do
+      before { get '/logs?page=2&per_page=30' }
+  
+      it 'Should return logs list using pagination' do
+        expect(json).not_to be_empty
+        expect(json.size).to eq(15)
+      end
+  
+      it 'Should return http status 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  let!(:role) { create(:role)}
+  let!(:users) { create_list(:user, 45, role_id: role.id) }
+
   describe "GET /users" do
     it "Should return http status 200" do
       get users_path
       expect(response).to have_http_status(200)
     end
+        
+    context 'GET /users?page=2&per_page=30' do
+      before { get '/users?page=2&per_page=30' }
+  
+      it 'Should return users list using pagination' do
+        expect(json).not_to be_empty
+        expect(json.size).to eq(15)
+      end
+  
+      it 'Should return http status 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
   end
 end
