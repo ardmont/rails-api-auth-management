b4d7ba18169e2c3b9fe3c13a951cae65e5e35ade
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-11-05 13:06:19 -0300

    Test GEt /user with filters.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   let!(:role) { create(:role)}
   let!(:users) { create_list(:user, 45, role_id: role.id) }
+  let(:user_sample) { users.sample }
 
   describe "GET /users" do
     it "Should return http status 200" do
@@ -17,10 +18,25 @@ RSpec.describe "Users", type: :request do
         expect(json).not_to be_empty
         expect(json.size).to eq(15)
       end
-  
-      it 'Should return http status 200' do
-        expect(response).to have_http_status(200)
+    end
+
+    context 'GET /users?name_eq=[name]' do 
+      before { get "/users?name_eq=#{user_sample.name}" }
+      
+      it 'Should return users by name' do
+        expect(json).not_to be_empty
+        expect(json[0]['name']).to eq(user_sample.name)
       end
+    end
+
+    context 'GET /users?role_id_eq=[role_id]' do 
+      before { get "/users?role_id_eq=#{user_sample.role.id}" }
+      
+      it 'Should return users by role' do
+        expect(json).not_to be_empty
+        expect(json[0]['role']['name']).to eq(user_sample.role.name)
+      end
+
     end
 
   end
