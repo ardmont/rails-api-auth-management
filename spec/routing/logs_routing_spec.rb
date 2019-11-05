731eaf9d0153148c9394e06bd70d693cca84b701
Ardiles Montenegro <ardiles.montenegro@gmail.com>
2019-11-04 22:19:18 -0300

    Eliminate unecessary actions from logs.

--- spec/routing/logs_routing_spec.rb
+++ spec/routing/logs_routing_spec.rb
@@ -9,22 +9,5 @@ RSpec.describe LogsController, type: :routing do
     it "routes to #show" do
       expect(:get => "/logs/1").to route_to("logs#show", :id => "1")
     end
-
-
-    it "routes to #create" do
-      expect(:post => "/logs").to route_to("logs#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(:put => "/logs/1").to route_to("logs#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/logs/1").to route_to("logs#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/logs/1").to route_to("logs#destroy", :id => "1")
-    end
   end
 end
